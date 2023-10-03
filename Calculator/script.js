d89f7133152f3c85a8a16a2ef94d8b998ccae7c7
govindy19 <130302812+govindy19@users.noreply.github.com>
2023-10-03 12:29:52 +0530

    Add files via upload

---
+++ Calculator/script.js
@@ -0,0 +1,29 @@
+let user_input= document.getElementById('user-input')
+let button =document.querySelectorAll('button')
+
+//console.log(button);
+
+button.forEach(element => {
+    element.addEventListener('click',(e) => {
+        console.log(e.target.textContent)
+        if(e.target.textContent ==="C"){
+            user_input.innerHTML = ""
+
+
+        }else if(e.target.textContent ==="X"){
+            user_input.innerHTML = user_input.innerHTML.slice(0,-1)
+
+
+
+        }
+        else if(e.target.textContent ==="="){
+            user_input.innerHTML = eval (user_input.innerHTML)
+
+        }
+        else{
+            user_input.innerHTML+= e.target.textContent
+        }
+        user_input.scrollLeft = user_input.scrollWidth
+    
+})
+})
